1db47e12919c0a79349774de88abae7fe5385d17
samt007 <samt007@qq.com>
2018-03-27 10:33:12 +0800

    modify the comm pom.xml

--- jebms-per/src/main/java/com/jebms/Application.java
+++ jebms-per/src/main/java/com/jebms/Application.java
@@ -3,8 +3,9 @@ package com.jebms;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author Sam.T
@@ -13,7 +14,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @RestController
 @SpringBootApplication
 @MapperScan(basePackages = "com.jebms.per.dao")
-public class Application extends WebMvcConfigurerAdapter {
+public class Application extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+	
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
