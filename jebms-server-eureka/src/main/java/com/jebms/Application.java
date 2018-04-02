51c8d63f71159c48e36f0071e952798cc971efe4
samt007 <samt007@qq.com>
2018-04-02 16:34:12 +0800

    for eureka test7

--- jebms-server-eureka/src/main/java/com/jebms/Application.java
+++ jebms-server-eureka/src/main/java/com/jebms/Application.java
@@ -2,11 +2,18 @@ package com.jebms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class Application {
+public class Application  extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
