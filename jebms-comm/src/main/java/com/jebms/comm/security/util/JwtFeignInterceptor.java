7061a70d2700551263472aad2800b51eb18f616c
samt007 <samt007@qq.com>
2018-04-03 16:29:48 +0800

    for the eureka security test2

--- jebms-comm/src/main/java/com/jebms/comm/security/util/JwtFeignInterceptor.java
+++ jebms-comm/src/main/java/com/jebms/comm/security/util/JwtFeignInterceptor.java
@@ -5,6 +5,7 @@ import feign.RequestTemplate;
 
 import org.springframework.stereotype.Component;
 
+import com.alibaba.fastjson.JSON;
 import com.jebms.comm.utils.TypeConverter;
 
 @Component
@@ -16,9 +17,10 @@ public class JwtFeignInterceptor implements RequestInterceptor {
 
         if (!template.headers().containsKey(key)) {
             String currentToken = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJXWDIxNDQ5MiIsImV4cCI6MTUyMzI2NTI3MX0.5_HLzm79ylKW6i8mV53LUDyb7g-4bqgL8ZQ-tkHP_WKISYe3GNuwd7Fo_u28b5CQBMrF0oXKxWPg9x7yGJRIoA";// UserUtils.getCurrentToken();
+            System.out.println(JSON.toJSONString(UserUtil.getCurrentUser()));
             if (!TypeConverter.isEmpty(currentToken)){
                 template.header(key, currentToken);
-                System.out.println("add the token for feign!");
+                System.out.println("add the token for feign:"+currentToken);
             }
         }
     }
